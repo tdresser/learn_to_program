f1396b52f396270e4e4d8cb3f117b094bc05793d
Tim Dresser <tdresser@gmail.com>
2015-05-06 21:35:41 -0400

    added skeleton

--- text_adventure.cc
+++ text_adventure.cc
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-struct Room {
-  bool is_wall;
+struct Location {
+  bool is_accessible;
   string description;
   int n;
   int e;
@@ -13,89 +13,104 @@ struct Room {
   int w;
 };
 
-const int NUM_ROOMS = 10;
-Room rooms[NUM_ROOMS];
-int room_count = 0;
+// Make an array of uninitialized locations.
+const int NUM_LOCATIONS = 10;
+Location locations[NUM_LOCATIONS];
 
-int makeRoom(bool is_wall, string description) {
-  if (!(room_count < NUM_ROOMS)) {
-    cout << "Increase NUM_ROOMS!";
+// The number of locations we've initialized.
+int initialized_location_count = 0;
+
+// Initialize a location in the next available slot in |locations|.
+int makeLocation(bool is_accessible, string description) {
+  if (!(initialized_location_count < NUM_LOCATIONS)) {
+    cout << "Increase NUM_LOCATIONS!";
   }
-  rooms[room_count].description = description;
-  rooms[room_count].is_wall = is_wall;
-  int room_id = room_count;
-  room_count += 1;
-  return room_id;
+
+  int location_id = initialized_location_count;
+  locations[location_id].description = description;
+  locations[location_id].is_accessible = is_accessible;
+  initialized_location_count += 1;
+  return location_id;
 }
 
 int main() {
-  int start =
-      makeRoom(false, "You arrive at the opening of a large cave. To the north "
-                      "there is a small stream. To the south is the exit of "
-                      "the cave. To the east "
-                      "and west, lies darkness.");
+  // Set up locations.
+  int start = makeLocation(
+      true, "You arrive at the opening of a large cave. To the north "
+            "there is a small stream. To the south is the exit of "
+            "the cave. To the east "
+            "and west, lies darkness.");
 
   int west_wall =
-      makeRoom(true, "You feel around to the west, but there's a wall of "
-                     "solid rock.");
+      makeLocation(false, "You feel around to the west, but there's a wall of "
+                          "solid rock.");
   int east_wall =
-      makeRoom(true, "You feel around to the west, but there's a wall of "
-                     "solid rock.");
-  int exit_path = makeRoom(true, "You don't want to leave. You just got here!");
+      makeLocation(false, "You feel around to the east, but there's a wall of "
+                          "solid rock.");
+  int exit_path =
+      makeLocation(false, "You don't want to leave. You just got here!");
 
-  int stream = makeRoom(
-      false, "As you near the stream, you see that it's teaming with "
-             "tiny little chimpanzees. You decide against taking a drink.");
-  rooms[start].n = stream;
-  rooms[start].e = east_wall;
-  rooms[start].s = exit_path;
-  rooms[start].w = west_wall;
+  int stream = makeLocation(
+      true, "As you near the stream, you see that it's teaming with "
+            "tiny little chimpanzees. You decide against taking a drink.");
+  locations[start].n = stream;
+  locations[start].e = east_wall;
+  locations[start].s = exit_path;
+  locations[start].w = west_wall;
 
-  int end = makeRoom(
-      false,
+  int end = makeLocation(
+      true,
       "The tiny chimpanzees begin to swarm around you. Without "
       "warning, they begin to climb over you. You are soon submerged, and run "
       "out of oxygen. You are dead.");
 
-  rooms[stream].n = end;
-  rooms[stream].e = east_wall;
-  rooms[stream].s = start;
-  rooms[stream].w = west_wall;
+  locations[stream].n = end;
+  locations[stream].e = east_wall;
+  locations[stream].s = start;
+  locations[stream].w = west_wall;
 
-  int dead = makeRoom(true, "Sorry, you're dead. You can't move any more.");
-  rooms[end].n = dead;
-  rooms[end].e = dead;
-  rooms[end].s = dead;
-  rooms[end].w = dead;
+  int dead =
+      makeLocation(false, "Sorry, you're dead. You can't move any more.");
+  locations[end].n = dead;
+  locations[end].e = dead;
+  locations[end].s = dead;
+  locations[end].w = dead;
 
-  int current;
-  current = start;
+  int current = start;
+
+  // Each run of this loop gets some input from the user, and responds to it.
   while (true) {
-    if (rooms[current].is_wall) {
-      cout << "How are we on a wall? Something bad is happening";
+    if (!locations[current].is_accessible) {
+      cout << "How are we in an inaccessible location? Something bad is "
+              "happening";
     }
-    cout << rooms[current].description << "\n";
+    // Write the description of the current location.
+    cout << locations[current].description << "\n";
+    // Get the user to enter in the direction they want to go, or "q" to quit.
     string dir;
     cin >> dir;
     if (dir == "q") {
       exit(1);
     }
-    int next;
+    // Try to go in the direction the user indicated. If the location isn't
+    // accessible, output that location's description, and don't update
+    // |current|.
+    int next = current;
     if (dir == "n") {
-      next = rooms[current].n;
+      next = locations[current].n;
     }
     if (dir == "e") {
-      next = rooms[current].e;
+      next = locations[current].e;
     }
     if (dir == "s") {
-      next = rooms[current].s;
+      next = locations[current].s;
     }
     if (dir == "w") {
-      next = rooms[current].w;
+      next = locations[current].w;
     }
 
-    if (rooms[next].is_wall) {
-      cout << rooms[next].description << "\n";
+    if (!locations[next].is_accessible) {
+      cout << locations[next].description << "\n";
     } else {
       current = next;
     }
