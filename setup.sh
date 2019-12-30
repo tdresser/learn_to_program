94655a2026ae8e509ded471ebca1282cdca9cb44
timpersonal <timpersonal@tdresser-macbookpro2.roam.corp.google.com>
2019-12-30 18:40:41 -0500

    Switching over to markdown, python, ordering, everything is different

--- setup.sh
+++
@@ -1,5 +0,0 @@
-cd $1
-wget https://atom.io/download/windows -O AtomSetup.exe
-chmod 777 AtomSetup.exe
-./AtomSetup.exe &
-mintty -
--- week4/bad_include.cc
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-#include "test_util.cc"
-
-using namespace std;
-
-int main() {
-  shouldBeTrue(false, "FAILURE");
-}
